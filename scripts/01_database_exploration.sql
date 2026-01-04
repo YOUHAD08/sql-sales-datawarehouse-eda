7d1d9aeb143c5ae2743b195e1fa3c96668f3b97a
Ayoub Youhad <ayoubyouhad79@gmail.com>
2026-01-04 23:01:19 +0100

    feat: explore database schema and catalog all tables
    Query INFORMATION_SCHEMA.TABLES to identify available data sources
    across Bronze, Silver, and Gold layers

---
+++ scripts/01_database_exploration.sql
@@ -0,0 +1,9 @@
+Use DataWarehouse
+
+-- Retrieve a list of all tables in the database
+SELECT 
+    TABLE_CATALOG, 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
